78a845ce45a236a4cc49f5f35877d6dd4edd5223
Dmitry Romashov <framebassman@gmail.com>
2020-09-21 10:57:19 +0300

    Feature/di (#2)
    * first implementation of DI
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- Nanny.Console/Printers/ConsolePrinter.cs
+++ Nanny.Console/Printers/ConsolePrinter.cs
@@ -1,17 +1,26 @@
+using Microsoft.Extensions.Logging;
 using Nanny.Console.Commands;
 
 namespace Nanny.Console.Printers
 {
     public class ConsolePrinter : Printer
     {
-        public ConsolePrinter(Command command) : base(command)
+        private ILogger<ConsolePrinter> _log;
+        
+        public ConsolePrinter(ILogger<ConsolePrinter> log, Command command) : base(command)
         {
-            
+            _log = log;
         }
 
         public override void Print()
         {
-            System.Console.WriteLine(Command.Output());
+            _log.LogInformation("Start printing");
+            WriteLine(Command.Output());
+        }
+
+        private void WriteLine(string output)
+        {
+            System.Console.WriteLine(output);
         }
     }
 }
--- Nanny.Console/Program.cs
+++ Nanny.Console/Program.cs
@@ -1,19 +1,55 @@
-﻿using Nanny.Console.Commands;
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nanny.Console.Commands;
 using Nanny.Console.Printers;
+using Serilog;
 
 namespace Nanny.Console
 {
     public class Program
     {
-        private CommandList _commands;
+        private IHost _host;
 
         public Program()
         {
-            _commands = new CommandList();
-            _commands.Add(new VersionCommand());
-            _commands.Add(new HelpCommand());
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+            try
+            {
+                Log.Logger.Information("Getting started...");
+                _host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddLogging(l =>
+                        {
+                            l.ClearProviders();
+                            l.AddSerilog(Log.Logger);
+                        });
+                        services.AddTransient<CommandList>();
+                        services.AddTransient<IPrinter, ConsolePrinter>();
+                    })
+                    .Build();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
-        
+
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -22,7 +58,12 @@ namespace Nanny.Console
 
         public void Run(string[] args)
         {
-            new ConsolePrinter(_commands.Find(args, new HelpCommand())).Print();
+            var list = ActivatorUtilities.CreateInstance<CommandList>(_host.Services);
+            ActivatorUtilities.CreateInstance<ConsolePrinter>(
+                _host.Services,
+                list.Find(args, new HelpCommand())
+                )
+                .Print();
         }
     }
 }
