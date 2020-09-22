8b3532b27f2a4889eb7244449d9ef271ef17d4e4
Dmitry Romashov <framebassman@gmail.com>
2020-09-22 16:01:35 +0300

    refactofing loader

--- Nanny.Console/Program.cs
+++ Nanny.Console/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Nanny.Console.Commands;
 using Nanny.Console.Printers;
 using Serilog;
@@ -14,47 +10,31 @@ namespace Nanny.Console
     public class Program
     {
         private IHost _host;
+        private ILogger _logger;
 
         public Program()
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Build())
-                .CreateLogger();
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddLogging(l =>
-                    {
-                        l.ClearProviders();
-                        l.AddSerilog(Log.Logger);
-                    });
-                    services.AddTransient<CommandList>();
-                    services.AddTransient<IPrinter, ConsolePrinter>();
-                })
-                .Build();
-        }
-
-        static void Main(string[] args)
-        {
             try
             {
-                Log.Logger.Information("Getting started...");
-                Program program = new Program();
-                program.Run(args);
+                Startup startup = new Startup();
+                _logger = startup.CreateLogger();
+                _logger.Information("Getting started...");
+                _host = startup.CreateHost(_logger);
             }
             catch (Exception ex)    
             {
-                Log.Logger.Fatal(ex, "Host terminated unexpectedly");
+                _logger.Fatal(ex, "Host terminated unexpectedly");
             }
             finally
             {
                 Log.CloseAndFlush();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run(args);
         }
 
         public void Run(string[] args)
