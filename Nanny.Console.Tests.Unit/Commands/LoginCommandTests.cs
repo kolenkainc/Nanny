92bde73e6f32cdbaa1ab87145c84b27eb7a31837
Dmitry Romashov <framebassman@gmail.com>
2020-09-24 23:16:59 +0300

    add test for success case

--- Nanny.Console.Tests.Unit/Commands/LoginCommandTests.cs
+++ Nanny.Console.Tests.Unit/Commands/LoginCommandTests.cs
@@ -12,20 +12,38 @@ namespace Nanny.Console.Tests.Unit.Commands
 {
     public class LoginCommandTests
     {
-        private LoginCommand _command;
-
-        public LoginCommandTests()
-        {
-            var mockContext = new Mock<ApplicationContext>();
-            mockContext.Setup(m => m.Properties).ReturnsDbSet(new List<Property>());
-            Mock<ILogger<LoginCommand>> loggerMock = new Mock<ILogger<LoginCommand>>();
-            _command = new LoginCommand(mockContext.Object, loggerMock.Object);
-        }
-
         [Fact]
         public void ExecuteCommand_WithEmptyDb_ReturnsErrorMessage()
         {
-            Assert.That(_command.Execute(), Is.EqualTo("Please, pass JiraToken"));
+            // Arrange
+            var mockContext = new Mock<ApplicationContext>();
+            mockContext.Setup(m => m.Properties).ReturnsDbSet(new List<Property>());
+            Mock<ILogger<LoginCommand>> loggerMock = new Mock<ILogger<LoginCommand>>();
+            LoginCommand command = new LoginCommand(mockContext.Object, loggerMock.Object);
+            
+            // Act and Assert
+            Assert.That(command.Execute(), Is.EqualTo("Please, pass JiraToken"));
+        }
+
+        [Fact]
+        public void ExecuteCommand_WithJiraToken_ReturnsOkMessage()
+        {
+            // Arrange
+            var mockContext = new Mock<ApplicationContext>();
+            var props = new List<Property>
+            {
+                new Property
+                {
+                    Key = "JiraToken",
+                    Value = "123"
+                }
+            };
+            mockContext.Setup(m => m.Properties).ReturnsDbSet(props);
+            Mock<ILogger<LoginCommand>> loggerMock = new Mock<ILogger<LoginCommand>>();
+            LoginCommand command = new LoginCommand(mockContext.Object, loggerMock.Object);
+            
+            // Act and Assert
+            Assert.That(command.Execute(), Is.EqualTo("You already have JiraToken"));
         }
     }
 }
