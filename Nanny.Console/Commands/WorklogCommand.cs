7f3de64167692ff10d7054734532cd4be0f0b3c3
Dmitry Romashov <framebassman@gmail.com>
2020-09-29 23:52:07 +0300

    validate jira info in worklog command

--- Nanny.Console/Commands/WorklogCommand.cs
+++ Nanny.Console/Commands/WorklogCommand.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Microsoft.Extensions.Logging;
+using Nanny.Console.Commands.Scenarios;
 using Nanny.Console.Database;
 using Nanny.Console.IO;
 
@@ -10,7 +11,10 @@ namespace Nanny.Console.Commands
         private Key _key = new Key("worklog", "w");
         private ILogger<WorklogCommand> _logger;
         private ApplicationContext _db;
+        private string JIRA_DOMAIN = "JiraDomain";
         private string JIRA_TOKEN = "JiraToken";
+        private Scenario _jiraDomainScenario;
+        private Scenario _jiraTokenScenario;
         private IScanner _scanner;
         
         public WorklogCommand(IPrinter printer, IScanner scanner, ILogger<WorklogCommand> logger, ApplicationContext db) : base(printer)
@@ -18,20 +22,18 @@ namespace Nanny.Console.Commands
             _logger = logger;
             _db = db;
             _scanner = scanner;
+            _jiraDomainScenario = new MissedKeyScenario(db, new Property{ Key = JIRA_DOMAIN }, printer, scanner, logger);
+            _jiraTokenScenario = new MissedKeyScenario(db, new Property{ Key = JIRA_TOKEN }, printer, scanner, logger);
         }
 
         public override void Execute()
         {
-            _logger.LogInformation($"Is {JIRA_TOKEN} exists?");
-            if (!_db.Properties.Any(p => p.Key == JIRA_TOKEN))
-            {
-                Printer.Print($"There is no stored {JIRA_TOKEN}. Please, login");
-                _logger.LogWarning($"There is no stored {JIRA_TOKEN}");
-                return;
-            }
-            _logger.LogInformation($"{JIRA_TOKEN} exist");
-
-            _logger.LogInformation("Start to scan directory for git structure");
+            _jiraDomainScenario.Execute();
+            _jiraTokenScenario.Execute();
+            _logger.LogInformation("Ask to task number");
+            Printer.Print("Type task number");
+            var task = _scanner.Scan();
+            _logger.LogInformation($"Task number is: {task}");
             
         }
 
