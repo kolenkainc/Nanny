d557ea81976777678f9b1fe947659246b35672d4
Dmitry Romashov <framebassman@gmail.com>
2020-09-30 23:34:16 +0300

    parse jira worklog to seconds

--- Nanny.Console/Commands/ExternalServices/WorklogRequest.cs
+++ Nanny.Console/Commands/ExternalServices/WorklogRequest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Nanny.Console.Commands.ExternalServices
 {
@@ -54,7 +56,25 @@ namespace Nanny.Console.Commands.ExternalServices
         {
             return _template3
                 .Replace("\'", "\"")
-                .Replace(_worklogPlaceholder, _worklog);
+                .Replace(_worklogPlaceholder, ParseHuman(_worklog).TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        public TimeSpan ParseHuman(string dateTime)
+        {
+            TimeSpan ts = TimeSpan.Zero;
+            string currentString = "";
+            string currentNumber = "";
+            foreach (char ch in dateTime+' ')
+            {
+                currentString += ch;
+                if (Regex.IsMatch(currentString, @"^(days(\d|\s)|day(\d|\s)|d(\d|\s))", RegexOptions.IgnoreCase)) { ts = ts.Add(TimeSpan.FromDays(int.Parse(currentNumber))); currentString = ""; currentNumber = ""; }
+                if (Regex.IsMatch(currentString, @"^(hours(\d|\s)|hour(\d|\s)|h(\d|\s))", RegexOptions.IgnoreCase)) { ts = ts.Add(TimeSpan.FromHours(int.Parse(currentNumber))); currentString = ""; currentNumber = ""; }
+                if (Regex.IsMatch(currentString, @"^(ms(\d|\s))", RegexOptions.IgnoreCase)) { ts = ts.Add(TimeSpan.FromMilliseconds(int.Parse(currentNumber))); currentString = ""; currentNumber = ""; }
+                if (Regex.IsMatch(currentString, @"^(mins(\d|\s)|min(\d|\s)|m(\d|\s))", RegexOptions.IgnoreCase)) { ts = ts.Add(TimeSpan.FromMinutes(int.Parse(currentNumber))); currentString = ""; currentNumber = ""; }
+                if (Regex.IsMatch(currentString, @"^(secs(\d|\s)|sec(\d|\s)|s(\d|\s))", RegexOptions.IgnoreCase)) { ts = ts.Add(TimeSpan.FromSeconds(int.Parse(currentNumber))); currentString = ""; currentNumber = ""; }
+                if (Regex.IsMatch(ch.ToString(), @"\d")) { currentNumber += ch; currentString = ""; }
+            }
+            return ts;
         }
     }
 }
