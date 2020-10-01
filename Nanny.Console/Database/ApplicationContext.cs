f79d3d2a9802950e5c69de851b86bc27d774b90a
Dmitry Romashov <framebassman@gmail.com>
2020-10-01 16:42:05 +0300

    add logging to ApplicationContext

--- Nanny.Console/Database/ApplicationContext.cs
+++ Nanny.Console/Database/ApplicationContext.cs
@@ -1,15 +1,28 @@
 using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Nanny.Console.IO;
 
 namespace Nanny.Console.Database
 {
     public class ApplicationContext : DbContext
     {
+        private ILoggerFactory _loggerFactory;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options, ILoggerFactory loggerFactory)
+            : base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+    
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
+            var logger = _loggerFactory.CreateLogger<ApplicationContext>();
             var fs = new FileSystem();
-            builder.UseSqlite($"Data Source={Path.Combine(fs.WorkingDirectory(), "nanny.db")}");
+            var connectionString = $"Data Source={Path.Combine(fs.WorkingDirectory(), "nanny.db")}";
+            logger.LogInformation("ConnectionString: {@connectionString}", connectionString);
+            builder.UseSqlite(connectionString);
+            builder.UseLoggerFactory(_loggerFactory);
         }
 
         public virtual DbSet<Property> Properties { get; set; }
--- Nanny.Console/Startup.cs
+++ Nanny.Console/Startup.cs
@@ -1,7 +1,5 @@
 using System;
-using System.IO;
 using System.Net.Http;
-using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
